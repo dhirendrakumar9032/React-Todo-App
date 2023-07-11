b87cfde3783f4cfc9212447e87ffbd045cb99cf2
Dhirendra Kumar <dhirendra.kumar@qapitacorp.com>
2023-07-12 01:17:52 +0530

    updated ui

--- src/components/To-do Items/index.tsx
+++ src/components/To-do Items/index.tsx
@@ -3,21 +3,34 @@ import { Checkbox } from "antd";
 import { DeleteFilled, EditFilled } from "@ant-design/icons";
 import { Todo } from "../../utils/interface";
 import "./index.css";
+import { CheckboxChangeEvent } from "antd/es/checkbox";
 
 interface ToDoItemProps {
   todo: Todo;
   handleDelete: (id: string) => void;
+  onEdit: (id: string) => void;
+  onCompleteTask: (id: string, isCompleted: boolean) => void;
 }
 
-const ToDoItem: FC<ToDoItemProps> = ({ todo, handleDelete }) => {
+const ToDoItem: FC<ToDoItemProps> = ({
+  todo,
+  handleDelete,
+  onEdit,
+  onCompleteTask,
+}) => {
+  const onChange = (e: CheckboxChangeEvent) => {
+    onCompleteTask(todo.id, e.target.checked);
+  };
+
   return (
-    <div className="to-do-item">
+    <div className={`to-do-item ${todo.isCompleted && "to-do-item-completed"}`}>
       <section>
-        <Checkbox checked={todo.isCompleted} />
+        <Checkbox checked={todo.isCompleted} onChange={onChange} />
         <span>{todo.todo}</span>
       </section>
       <section>
-        <EditFilled />
+        <EditFilled onClick={() => onEdit(todo.id)} />
+
         <DeleteFilled onClick={() => handleDelete(todo.id)} />
       </section>
     </div>
--- src/components/To-do List/index.tsx
+++ src/components/To-do List/index.tsx
@@ -4,9 +4,14 @@ import { Button } from "antd";
 import { ToDoItem } from "../To-do Items";
 import { Todo } from "../../utils/interface";
 import { generateUniqueId } from "../../utils/helper";
+import { log } from "console";
 
 const ToDoList = () => {
   const [inputText, setInputText] = useState<string>("");
+  const [isEditable, setEditable] = useState<any>({
+    id: "id",
+    edit: false,
+  });
   const [todos, setTodos] = useState<Todo[]>([]);
   const [completedTask, setCompletedTask] = useState<number>(0);
 
@@ -15,14 +20,29 @@ const ToDoList = () => {
   };
 
   const handleAddTodo = () => {
-    const newTodo: Todo = {
-      isCompleted: false,
-      todo: inputText,
-      id: generateUniqueId(),
-    };
+    if (!isEditable.edit) {
+      const newTodo: Todo = {
+        isCompleted: false,
+        todo: inputText,
+        id: generateUniqueId(),
+      };
 
-    setTodos((prevTodos) => [...prevTodos, newTodo]);
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+    } else {
+      const newTodoList = todos.map((todo) => {
+        if (todo.id === isEditable.id) {
+          return {
+            ...todo,
+            todo: inputText,
+          };
+        } else {
+          return todo;
+        }
+      });
+      setTodos(newTodoList);
+    }
     setInputText("");
+    setEditable({ id: "", edit: false });
   };
 
   const handleCompleteAll = () => {
@@ -39,37 +59,71 @@ const ToDoList = () => {
     setTodos(updatedTodos);
   };
 
+  const onCompleteTask = (id: string, isCompleted: boolean) => {
+    const newTodoList = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isCompleted: isCompleted,
+        };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newTodoList);
+  };
+
+  const onEdit = (id: string) => {
+    const editText = todos.find((todo) => todo.id === id)?.todo;
+    editText && setInputText(editText);
+    setEditable({ id, edit: true });
+  };
+
   useEffect(() => {
     const totalCompleted = todos.filter((todo) => todo.isCompleted);
     setCompletedTask(totalCompleted.length);
   }, [todos]);
 
   return (
-    <div className="to-do-list">
+    <div className="main-container">
       <h1>To-do List</h1>
       <section className="input-field">
         <input
           type="text"
-          placeholder="add to do "
+          placeholder="Please write here..."
           value={inputText}
           onChange={handleChange}
         />
-        <button onClick={handleAddTodo}>Add</button>
+        <button onClick={handleAddTodo}>
+          {isEditable.edit ? "Update" : "Add Todo"}
+        </button>
       </section>
-      <section className="filter-btn">
-        <Button onClick={handleCompleteAll}>Complete All Task</Button>
-        <Button onClick={handleDeleteAll}>Delete To-Do Items</Button>
-      </section>
-      <section className="to-do-items">
-        {todos.map((todo) => (
-          <ToDoItem key={todo.id} todo={todo} handleDelete={handleDelete} />
-        ))}
-      </section>
-      <section className="footer">
-        <Button>Filter</Button>
-        <h5>Completed:{completedTask}</h5>
-        <p>Total Task: {todos.length}</p>
-      </section>
+      <div className="to-do-list">
+        <section className="filter-btn">
+          <Button type="primary" onClick={handleCompleteAll}>
+            Complete All Task
+          </Button>
+          <Button type="primary" onClick={handleDeleteAll}>
+            Delete To-Do Items
+          </Button>
+        </section>
+        <section className="to-do-items">
+          {todos.map((todo) => (
+            <ToDoItem
+              key={todo.id}
+              todo={todo}
+              handleDelete={handleDelete}
+              onCompleteTask={onCompleteTask}
+              onEdit={onEdit}
+            />
+          ))}
+        </section>
+        <section className="footer">
+          <Button>Filter</Button>
+          <h5>Completed:{completedTask}</h5>
+          <p>Total Task: {todos.length}</p>
+        </section>
+      </div>
     </div>
   );
 };
